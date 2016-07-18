cfe6cae2f94de0c50d7f96470947704beef2494d
Kohei Tabata <nerd0geek1@gmail.com>
2016-07-18 23:03:24 +0900

    add PlayListPlayerType as PlayListPlayer interface

---
+++ PlayListPlayer/classes/player/PlayListPlayerType.swift
@@ -0,0 +1,33 @@
+//
+//  PlayListPlayerType.swift
+//  PlayListPlayer
+//
+//  Created by Kohei Tabata on 7/18/16.
+//  Copyright © 2016 nerd0geek1. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+public protocol PlayListPlayerType {
+
+    var didFinishPlayingTrack:(() -> Void)? { get set }
+    var didFinishPlayingPlayList:(() -> Void)? { get set }
+
+    func setPlayList(urls: [NSURL])
+    func setCurrentIndex(index: Int) -> Bool
+
+    func engine() -> AVPlayer
+    func hasPlayList() -> Bool
+    func currentTrackURL() -> NSURL?
+    func isPlaying() -> Bool
+
+    func play()
+    func pause()
+    func beginFastForwarding()
+    func endFastForwarding()
+    func skipToNextTrack()
+    func beginRewinding()
+    func endRewinding()
+    func jumpToPreviousTrack()
+}
