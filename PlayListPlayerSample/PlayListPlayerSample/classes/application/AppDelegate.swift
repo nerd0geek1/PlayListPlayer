3743f7c40cd3016db9d8aa2883cbc1c180f02789
Kohei Tabata <nerd0geek1@gmail.com>
2016-08-03 10:12:24 +0900

    organize sample project packaging

---
+++ PlayListPlayerSample/PlayListPlayerSample/classes/application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  PlayListPlayerSample
+//
+//  Created by Kohei Tabata on 8/2/16.
+//  Copyright © 2016 Kohei Tabata. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
