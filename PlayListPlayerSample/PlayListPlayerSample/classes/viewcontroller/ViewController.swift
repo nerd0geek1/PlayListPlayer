7562fa0cb1fbc5acbcb885a16c8e96bc55743fa3
Kohei Tabata <nerd0geek1@gmail.com>
2016-08-03 10:50:33 +0900

    implement sample project

--- PlayListPlayerSample/PlayListPlayerSample/classes/viewcontroller/ViewController.swift
+++ PlayListPlayerSample/PlayListPlayerSample/classes/viewcontroller/ViewController.swift
@@ -7,10 +7,55 @@
 //
 
 import UIKit
+import PlayListPlayer
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var movieRenderingView: MovieRenderingView!
+
+    private let player: PlayListPlayer = PlayListPlayer.sharedInstance
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupPlayer()
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+
+        player.play()
+    }
+
+    //MARK: - private
+
+    private func setupPlayer() {
+        let url1: NSURL = NSBundle.mainBundle().URLForResource("sample_audio1", withExtension: "mp3")!
+        let url2: NSURL = NSBundle.mainBundle().URLForResource("sample_audio2", withExtension: "mp3")!
+        let url3: NSURL = NSBundle.mainBundle().URLForResource("sample_movie", withExtension: "mp4")!
+
+        player.setPlayList([url1, url2, url3])
+        movieRenderingView.setPlayer(player)
+    }
+
+    //MARK: - IBAction
+
+    @IBAction
+    func tapPrevious() {
+        player.jumpToPreviousTrack()
+    }
+
+    @IBAction
+    func tapPlay() {
+        if player.isPlaying() {
+            player.pause()
+        } else {
+            player.play()
+        }
+    }
+
+    @IBAction
+    func tapNext() {
+        player.skipToNextTrack()
     }
 }
